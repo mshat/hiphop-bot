882a8264bde883876957eedc1ac987302ae33950
Maksim Shatokhin <max.sh99@mail.ru>
2022-03-01 23:24:02 +0300

    перенёс функции вывода в консоль из interface.py в console_interface.view.py

--- hiphop_bot/console_interface/view.py
+++ hiphop_bot/console_interface/view.py
@@ -1,9 +1,16 @@
-from hiphop_bot.dialog_bot.query_solving.dialog import Dialog, DialogState
+from typing import Iterable, List
+from hiphop_bot.dialog_bot.query_solving.dialog import Dialog
 from hiphop_bot.dialog_bot.query_solving.user import User
 from hiphop_bot.dialog_bot.recommender_system import filter
-from hiphop_bot.dialog_bot.recommender_system import interface
 from hiphop_bot.dialog_bot.config import DEBUG, ENABLE_FILTERS
 from hiphop_bot.dialog_bot.data.const import LINE_LEN
+
+
+def trunc_output(output: Iterable, output_len=None) -> List:
+    output = list(output)
+    if output_len is None:
+        output_len = 100000
+    return output[:output_len]
 
 
 def print_after_search_message():
@@ -26,32 +33,45 @@ def filter_search_result(user: User, dialog: Dialog):
         )
 
 
-def show_recommendations(user: User, dialog: Dialog):
+def print_recommendations(user: User, dialog: Dialog):
     filtered = filter_search_result(user, dialog)
     if filtered:
-        artists = filtered
+        recommended_artists = filtered
     else:
-        artists = dialog.search_result
+        recommended_artists = dialog.search_result
 
-    if artists:
+    if recommended_artists:
         if user.dislikes:
             print(f'Список дизлайков: {", ".join(user.dislikes)}')
         if user.str_filters != '':
             print(f'Установлены фильтры: {user.str_filters}')
 
-        interface.print_recommendations(artists, output_len=user.output_len, debug=DEBUG)
+        recommended_artists = trunc_output(recommended_artists, user.output_len)
+
+        for artist_name in recommended_artists:
+            if DEBUG:
+                print(artist_name, recommended_artists[artist_name])
+            else:
+                print(artist_name)
 
 
-def show_artists(user: User, dialog: Dialog):
+def print_artists(user: User, dialog: Dialog):
     artists = dialog.output_artists
+    artists = trunc_output(artists, user.output_len)
     if artists:
-        interface.print_artists(artists, max_output_len=user.output_len, debug=DEBUG)
+        for i, artist in enumerate(artists):
+            if DEBUG:
+                print(artist.name, artist.genre)
+            else:
+                print(artist.name)
 
 
-def show_genres(user: User, dialog: Dialog):
-    messages = dialog.output_genres
-    if messages:
-        interface.print_messages(messages, max_output_len=user.output_len)
+def print_genres(user: User, dialog: Dialog):
+    genres = dialog.output_genres
+    genres = trunc_output(genres, user.output_len)
+    if genres:
+        for genre in genres:
+            print(genre)
 
 
 class ConsolePrinter:
@@ -82,16 +102,16 @@ class ConsolePrinter:
             self.dialog.debug_message = ''
 
         if self.dialog.search_result:
-            show_recommendations(self.user, self.dialog)
+            print_recommendations(self.user, self.dialog)
             self.dialog.search_result = []
             print_after_search_message()
 
         if self.dialog.output_artists:
-            show_artists(self.user, self.dialog)
+            print_artists(self.user, self.dialog)
             self.dialog.output_artists = []
 
         if self.dialog.output_genres:
-            show_genres(self.user, self.dialog)
+            print_genres(self.user, self.dialog)
             self.dialog.output_genres = []
 
         if self.dialog.output_message:
