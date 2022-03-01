37df7ac40ae1c9a06e59786153200800ab5c7a57
Maksim Shatokhin <max.sh99@mail.ru>
2022-03-02 01:21:54 +0300

    перенёс формирование ответного сообщения во вью

--- hiphop_bot/telegram_interface/main.py
+++ hiphop_bot/telegram_interface/main.py
@@ -1,10 +1,9 @@
 import telebot
 from hiphop_bot.dialog_bot.sentence_analyzer.sentence_parser import SentenceParser
-from hiphop_bot.dialog_bot.query_solving.query_solver import QuerySolver
+from hiphop_bot.dialog_bot.query_solving.query_solver import QuerySolver, SOLVED, UNSOLVED
 from hiphop_bot.dialog_bot.query_solving.user import User
-from hiphop_bot.dialog_bot.query_solving.dialog import DialogState
-from hiphop_bot.dialog_bot.config import DEBUG
 from hiphop_bot.dialog_bot.data.const import LINE_LEN
+from hiphop_bot.telegram_interface.view import AnswerGenerator
 
 TOKEN = '5168804721:AAGBSsgGVMV5JQ258fnm6O6N96EXKwwkL3I'
 
@@ -12,31 +11,49 @@ bot = telebot.TeleBot(TOKEN)
 
 user = User()
 query_solver = QuerySolver(user)
+answer_generator = AnswerGenerator()
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    print(f'[DEBUG] {message.text}')
+    print(f'[USER MESSAGE] {message.text}')
     if message.text == "/start":
-        msg = (f'{"=" * LINE_LEN}\n'
-               'Вас приветствует разговорный бот.\n'
-               'Я кое-что знаю о русском хип-хопе и готов ответить на ваши вопросы по этой теме.\n'
-               'Вы можете узнать о моих возможностях, спросив меня об этом.\n'
-               f'{"=" * LINE_LEN}'
-               )
-        bot.send_message(message.from_user.id, msg)
+        bot.send_message(message.from_user.id, start_answer())
     elif message.text == '':
-        bot.send_message(message.from_user.id, 'Вы что-то хотели?..')
+        bot.send_message(message.from_user.id, blank_answer())
     else:
-        reply = solve_message(message.text + '\n')
-        bot.send_message(message.from_user.id, reply)
+        reply = solve_message(message.text)
+        if reply != '':
+            bot.send_message(message.from_user.id, reply)
+
+
+def start_answer():
+    msg = (f'{"=" * LINE_LEN}\n'
+           'Вас приветствует разговорный бот.\n'
+           'Я кое-что знаю о русском хип-хопе и готов ответить на ваши вопросы по этой теме.\n'
+           'Вы можете узнать о моих возможностях, спросив меня об этом.\n'
+           f'{"=" * LINE_LEN}'
+           )
+    return msg
+
+
+def blank_answer():
+    msg = 'Вы что-то хотели?..'
+    return msg
 
 
 def solve_message(sentence: str) -> str:
     query = SentenceParser(sentence).parse(query_solver.state)
-    query_solver.solve(query)
-    if DEBUG: print('[CURRENT STATE]', query_solver.state)
-    return query_solver.dialog.query_result
+    res = query_solver.solve(query)
 
+    if res == SOLVED:
+        answer_generator.user = query_solver.user
+        answer_generator.dialog = query_solver.dialog
+
+        return answer_generator.generate_answer()
+    elif res == UNSOLVED:
+        return 'Я вас не понял :('
+    else:
+        raise Exception('Unknown query_solver result')
 
 bot.polling(none_stop=True, interval=0)
