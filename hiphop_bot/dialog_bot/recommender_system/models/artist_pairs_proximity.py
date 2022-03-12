8000f75ac760c0d748291acef9aa672b336bdeff
Maksim Shatokhin <max.sh99@mail.ru>
2022-03-12 19:09:37 +0300

    изменил ArtistPairsProximity. Теперь класс имеет одно поле artists_proximity, содержащее словарь близости артистов вида {artist1: {artist2: 0.3, artist3: 0.2}, artist2...}

--- hiphop_bot/dialog_bot/recommender_system/models/artist_pairs_proximity.py
+++ hiphop_bot/dialog_bot/recommender_system/models/artist_pairs_proximity.py
@@ -1,19 +1,31 @@
-from typing import List
-
-from hiphop_bot.db.model import Model
+from typing import List, Tuple, Dict
+from hiphop_bot.db.model import Model, ModelError
 
 
 class ArtistPairsProximity:
-    def __init__(self, first_artist: str, second_artist: str, proximity: float):
-        self.first_artist = first_artist
-        self.second_artist = second_artist
-        self.proximity = proximity
+    artists_proximity: Dict[str, Dict[str, float]]  # {artist1: {artist2: 0.3, artist3: 0.2}, artist2...}
+
+    def __init__(self, raw_data: List[Tuple[int, str, str, float]]):
+        self.artists_proximity = {}
+        for first_artist, second_artist, proximity in raw_data:
+            if first_artist in self.artists_proximity:
+                self.artists_proximity[first_artist].update({second_artist: proximity})
+            else:
+                self.artists_proximity[first_artist] = {second_artist: proximity}
+
+        self._pairs_len = len(raw_data)
+        
+    def get_proximity(self, first_artist: str, second_artist: str) -> float:
+        try:
+            return self.artists_proximity[first_artist][second_artist]
+        except KeyError as e:
+            raise Exception(f'Artist not found: {e}')
 
     def __str__(self):
-        return f'{self.first_artist} {self.second_artist} {self.proximity}'
+        return f'ArtistPairsProximity: with {self._pairs_len} pairs'
 
     def __repr__(self):
-        return f'ArtistPairsProximity: "{self.__str__()}"'
+        return self.__str__()
 
 
 class ArtistPairsProximityModel(Model):
@@ -27,7 +39,24 @@ class ArtistPairsProximityModel(Model):
             f"inner join artist as art2 on {self._table_name}.second_artist_id = art2.id "
         )
 
-    def get_all(self) -> List[ArtistPairsProximity]:
+    def _convert_to_objects(self, raw_data: List[Tuple]) -> ArtistPairsProximity:
+        """
+        Преобразует список кортежей в объект класса ArtistPairsProximity
+        """
+        try:
+            object_ = self._model_class(raw_data)
+            return object_
+        except TypeError as e:
+            raise ModelError(f'Conversion type error: {e}')
+        except Exception as e:
+            raise ModelError(f'Unknown conversion error: {e}')
+
+    def _select(self, query) -> ArtistPairsProximity:
+        raw_data = self._raw_select(query)
+        objects = self._convert_to_objects(raw_data)
+        return objects
+
+    def get_all(self) -> ArtistPairsProximity:
         genres = self._select(self._get_all_query)
         return genres
 
