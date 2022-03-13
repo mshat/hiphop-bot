1a30ea3697677e9359609c93b0bb1cd3e55adeff
Maksim Shatokhin <max.sh99@mail.ru>
2022-03-13 19:10:19 +0300

    добавил объект с ссылками на стриминги в модель _Artist и дописал соответствующую логику в ArtistModel

--- hiphop_bot/dialog_bot/recommender_system/models/artist.py
+++ hiphop_bot/dialog_bot/recommender_system/models/artist.py
@@ -1,14 +1,17 @@
-from typing import List, Tuple
-from hiphop_bot.db.model import Model
+from typing import List, Tuple, Dict
+from hiphop_bot.db.model import Model, ModelError
 from hiphop_bot.dialog_bot.recommender_system.models.theme import _Theme  # импортирутеся для аннотации
 from hiphop_bot.dialog_bot.recommender_system.models.gender import _Gender  # импортирутеся для аннотации
 from hiphop_bot.dialog_bot.recommender_system.models.genre import _Genre  # импортирутеся для аннотации
+from hiphop_bot.dialog_bot.recommender_system.models.artist_streaming_service_link import (
+    ArtistStreamingServiceLinkModel, _StreamingServiceLinks)
 
 
 class _Artist:
     theme: _Theme
     gender: _Gender
     genre: _Genre
+    streaming_service_links: _StreamingServiceLinks | None
 
     def __init__(
             self,
@@ -18,6 +21,7 @@ class _Artist:
             theme: _Theme | str,
             gender: _Gender | str,
             genre: _Genre | str,
+            streaming_service_links: _StreamingServiceLinks = None,
     ):
         self.name = name
         self.year_of_birth = year_of_birth
@@ -25,6 +29,7 @@ class _Artist:
         self._theme = theme if isinstance(theme, _Theme) else _Theme(theme)
         self._gender = gender if isinstance(gender, _Gender) else _Gender(gender)
         self._genre = genre if isinstance(genre, _Genre) else _Genre(genre)
+        self._streaming_service_links = streaming_service_links
 
     @property
     def theme(self):
@@ -38,9 +43,20 @@ class _Artist:
     def genre(self):
         return self._genre.name
 
+    @property
+    def streaming_service_links(self):
+        return self._streaming_service_links
+
+    @streaming_service_links.setter
+    def streaming_service_links(self, links: _StreamingServiceLinks):
+        if isinstance(links, _StreamingServiceLinks):
+            self._streaming_service_links = links
+        else:
+            raise ModelError("Argument must have _StreamingServiceLinks type")
+
     def __str__(self):
         return f'{self.name} {self.year_of_birth} {self.group_members_number} {self._theme} {self._gender} ' \
-               f'{self._genre}'
+               f'{self._genre} {self.streaming_service_links}'
 
     def __repr__(self):
         return f'Artist: "{self.__str__()}"'
@@ -58,8 +74,20 @@ class ArtistModel(Model):
             "inner join genre on artist.genre_id = genre.id "
         )
 
+    def _select_model_objects(self, query) -> List[_Artist]:
+        artists: List[_Artist] = super()._select_model_objects(query)
+
+        streaming_service_link_model = ArtistStreamingServiceLinkModel()
+        artist_links = streaming_service_link_model.get_artist_links_dict()
+
+        # добавляем артистам ссылки на стриминговые сервисы
+        for artist in artists:
+            if artist.name in artist_links:
+                artist.streaming_service_links = artist_links[artist.name]
+        return artists
+
     def get_all(self) -> List[_Artist]:
-        artists = self._select(self._get_all_query)
+        artists = self._select_model_objects(self._get_all_query)
         return artists
 
     def get_all_raw(self) -> List[Tuple]:
@@ -74,5 +102,10 @@ class ArtistModel(Model):
     def get_by_genre(self, genre) -> List[_Artist] | List:
         query = self._get_all_query + \
                 f"where genre.name = '{genre}'"
-        artists = self._select(query)
+        artists = self._select_model_objects(query)
         return artists
+
+
+m = ArtistModel()
+for item in m.get_all():
+    print(item)
