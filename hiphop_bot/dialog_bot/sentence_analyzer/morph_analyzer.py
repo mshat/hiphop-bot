a51f3b918b0998dedeb6e1d2029c03123d9fcc30
Maksim Shatokhin <max.sh99@mail.ru>
2022-03-14 17:45:36 +0300

    добавил приведение слова к нормальной форме для отладки в morph_analyzer.py

--- hiphop_bot/dialog_bot/sentence_analyzer/morph_analyzer.py
+++ hiphop_bot/dialog_bot/sentence_analyzer/morph_analyzer.py
@@ -1,3 +1,9 @@
 import pymorphy2
 
 MorphAnalyzer = pymorphy2.MorphAnalyzer()
+
+if __name__ == '__main__':
+    word = 'сбрось'
+    parsed_word = MorphAnalyzer.parse(word)[0]
+    print(parsed_word)
+
