4d6e82b3417eb4357966d65c40b3903df390bbe8
Maksim Shatokhin <max.sh99@mail.ru>
2022-03-16 19:56:45 +0300

    пофиксил фильтрацию в AdaptedRecommenderSystem. Теперь в метод filter_artists в аргумент artists можно передавать как List[ArtistNode], так и List[_Artist]

--- hiphop_bot/dialog_bot/services/recommender_system_class_adapter.py
+++ hiphop_bot/dialog_bot/services/recommender_system_class_adapter.py
@@ -1,7 +1,7 @@
 from typing import List
 from hiphop_bot.recommender_system.recommender_system import RecommenderSystem
 from hiphop_bot.recommender_system.models.artist import _Artist  # Импортируется для аннотаций
-from hiphop_bot.recommender_system.tree.artist_node import ArtistVisualNode
+from hiphop_bot.recommender_system.tree.artist_node import ArtistNode
 
 
 class AdaptedRecommenderSystem:
@@ -14,39 +14,53 @@ class AdaptedRecommenderSystem:
 
     def recommend_by_seed(self, seed_artist: str, disliked_artists: List[str], debug=False) -> List[_Artist]:
         recommendations_by_artist = self.recommender_system.recommend_by_seed(seed_artist, disliked_artists, debug)
-        adapted_recommendations_by_artist = self._adapt_artists(recommendations_by_artist)
+        adapted_recommendations_by_artist = self._adapt_artists_to_artist_type(recommendations_by_artist)
         return adapted_recommendations_by_artist
 
     def recommend_by_likes(self, liked_artists: List[str], disliked_artists: List[str], debug=False) -> List[_Artist]:
         recommendations_by_likes = self.recommender_system.recommend_by_likes(liked_artists, disliked_artists, debug)
-        adapted_recommendations_by_likes = self._adapt_artists(recommendations_by_likes)
+        adapted_recommendations_by_likes = self._adapt_artists_to_artist_type(recommendations_by_likes)
         return adapted_recommendations_by_likes
 
-    def _adapt_artists(self, artists: List[ArtistVisualNode | _Artist]) -> List[_Artist]:
+    def _adapt_artists_to_artist_type(self, artists: List[ArtistNode | _Artist]) -> List[_Artist]:
         adapted_artists = []
 
         for artist_obj in artists:
-            if isinstance(artist_obj, ArtistVisualNode):
+            if isinstance(artist_obj, ArtistNode):
                 adapted_artists.append(artist_obj.artist)
             elif isinstance(artist_obj, _Artist):
                 adapted_artists.append(artist_obj)
         return adapted_artists
 
+    def _adapt_artists_to_artist_visual_node_type(self, artists: List[ArtistNode | _Artist]) \
+            -> List[ArtistNode]:
+        adapted_artists = []
+
+        for artist_obj in artists:
+            if isinstance(artist_obj, ArtistNode):
+                adapted_artists.append(artist_obj)
+            elif isinstance(artist_obj, _Artist):
+                adapted_artists.append(ArtistNode(artist_obj))
+        return adapted_artists
+
     def get_all_artists(self) -> List[_Artist]:
         artists = self.recommender_system.get_all_artists()
-        return self._adapt_artists(artists)
+        return self._adapt_artists_to_artist_type(artists)
 
     def get_artists_by_genre(self, genre: str) -> List[_Artist]:
         artists = self.recommender_system.get_artists_by_genre(genre)
-        return self._adapt_artists(artists)
+        return self._adapt_artists_to_artist_type(artists)
 
     def filter_artists(
             self,
-            artists: List[ArtistVisualNode] | List[_Artist],
+            artists: List[ArtistNode] | List[_Artist],
             group_type: str = 'any',
             sex: str = 'anysex',
             younger: int = None,
             older: int = None) -> List[_Artist]:
-        filtered = self.recommender_system.filter_artists(artists, group_type, sex, younger, older)
-        adapted_filtered = self._adapt_artists(filtered)
-        return adapted_filtered
+        adapted_for_recommender_system_artists = self._adapt_artists_to_artist_visual_node_type(artists)
+        filtered_artists = self.recommender_system.filter_artists(
+            adapted_for_recommender_system_artists, group_type, sex, younger, older
+        )
+        adapted_filtered_artists = self._adapt_artists_to_artist_type(filtered_artists)
+        return adapted_filtered_artists
