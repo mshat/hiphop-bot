b4f48f6600b378cd45348755a8ab3a887f91e6d7
Maksim Shatokhin <max.sh99@mail.ru>
2022-03-17 20:03:10 +0300

    заменил вывод спика жанров (обработчик ShowAllGenresHandler) на захардкоженное дерево жанров

--- hiphop_bot/dialog_bot/services/query_handling/handlers/genres.py
+++ hiphop_bot/dialog_bot/services/query_handling/handlers/genres.py
@@ -3,21 +3,46 @@ from hiphop_bot.dialog_bot.services.query_handling.query_handler import QueryHan
 from hiphop_bot.dialog_bot.services.query_solving.dialog import Dialog, DialogState
 from hiphop_bot.dialog_bot.services.query_solving.user import User
 from hiphop_bot.dialog_bot.services.sentence_analyzer.query import Query
-from hiphop_bot.dialog_bot.models.data import GENRES
 from hiphop_bot.dialog_bot.services.query_handling.tag_condition import (OrTagCondition as Or,
-                                                                         AndMultiTagCondition as AndMulti)
+                                                                         AndMultiTagCondition as AndMulti,
+                                                                         AndTagCondition as And)
 
 
 class ShowAllGenresHandler(QueryHandler):
     def __init__(self):
         super().__init__()
-        self.conditions = [AndMulti([Or('all'), Or('like/how')]), AndMulti([Or('include'), Or('genre'), Or('show')])]
+        self.conditions = [And('genre'), AndMulti([Or('all'), Or('like/how'), Or('tree')])]
         self.debug_msg = 'Вывести все жанры в базе'
 
     def handle(self, query: Query, user: User, dialog: Dialog):
-        genres = set(GENRES.values())
-        dialog.found_genres = genres
-        dialog.info = 'Кстати, в фильтрах вы можете указывать название жанра на русском языке'
+        genres_tree = """
+Новая школа:
+- Альтернатива
+--- Эмо
+--- Рэп-рок
+- Электронный хип-хоп
+--- Клауд
+--- Клубный
+--- Дрилл
+--- Грайм
+--- Мамбл
+--- Фонк
+- Хардкор
+--- Хорроркор
+--- Рэпкор
+--- Андеграунд
+- Поп-рэп
+--- Кальянный
+--- Поп
+
+Старая школа:
+- Олдскул хардкор
+--- Гангста
+--- Спортивный
+- Русский рэп
+"""
+        dialog.info = 'Дерево жанров русской хип-хоп музыки по моей версии:\n'
+        dialog.info += f'{genres_tree}\n'
         return DialogState.START
 
 
