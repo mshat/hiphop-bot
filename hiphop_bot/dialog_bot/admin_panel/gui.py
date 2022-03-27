6ffe419240175f6b0c67dcf3ab2d1f4ba8528f95
Maksim Shatokhin <max.sh99@mail.ru>
2022-03-27 14:05:10 +0300

    добавил метод update_artist и таблиу с артистами

--- hiphop_bot/dialog_bot/admin_panel/gui.py
+++ hiphop_bot/dialog_bot/admin_panel/gui.py
@@ -1,15 +1,17 @@
 import threading
 from time import sleep
-from typing import Callable
+from typing import Callable, List
 import validators
 import PySimpleGUI as sg
-from hiphop_bot.recommender_system.models.artist import ArtistModel
+from hiphop_bot.recommender_system.models.artist import ArtistModel, _Artist  # Импортируется для аннотаций
+from hiphop_bot.db.abstract_model import AlreadyInTheDatabaseError
 
 
 THEMES_FIELDS = ['_hard-gangsta_', '_workout_theme_', '_soft-gangsta_', '_feelings_', '_fun_', '_art_', '_conscious_']
 GENRES_FIELDS = ['_alternative_', '_emo_', '_raprock_', '_electronichiphop_', '_cloud_', '_club_', '_drill_', '_grime_',
                  '_mumble_', '_phonk_', '_hardcore_', '_horrorcore_', '_rapcore_', '_underground_', '_popular_',
-                 '_hookah_', '_pop_', '_oldschoolhardcore_', '_gangsta_', '_workout_', '_russianrap_']
+                 '_hookah_', '_pop_', '_oldschoolhardcore_', '_gangsta_', '_workout_', '_russianrap_', '_classic_',
+                 '_soft_']
 STREAMING_SERVICES_FIELDS = ['_spotify_', '_boom_', '_yandex_']
 
 alternative_frame_layout = sg.Frame('Альтернатива', [[
@@ -51,6 +53,8 @@ old_school_hardcore_frame_layout = sg.Frame('Олдскул хардкор', [[
 
 russian_rap_layout = sg.Frame('Русский рэп', [[
         sg.Checkbox(text='Русский рэп', key='_russianrap_'),
+        sg.Checkbox(text='Классический', key='_classic_'),
+        sg.Checkbox(text='Чувства', key='_soft_'),
 ]])
 
 
@@ -115,25 +119,28 @@ artist_data_frame = sg.Frame('Artist', [
             ])
     ],
     [
-        sg.Frame('Имя артиста в разных падежах (через запятую)', [[
-            sg.InputText(default_text='', key='_aliases_', size=(79, 1))
-        ]])
+        sg.Frame('Имя на русском в разных падежах (через запятую) (по вопросам: что? нет кого? кому? вижу кого?)',
+                 [[sg.InputText(default_text='', key='_aliases_', size=(80, 1))]])
     ],
 ])
 
 add_frame = sg.Frame('Add', [
-    [sg.Button('Очистить поля ввода'), sg.Submit(button_text='Добавить'), sg.Cancel()],
+    [sg.Button('Очистить поля ввода'), sg.Button(button_text='Добавить'), sg.Cancel()],
     [
         sg.Multiline(size=(100, 10), key='_add_output_'),
     ]
-], size=(600, 240))
+], size=(600, 200))
 
 update_frame = sg.Frame('Update', [
-    [sg.Button('Очистить поля ввода'), sg.Submit(button_text='Добавить'), sg.Cancel()],
+    [sg.Button(button_text='Обновить')],
     [
-        sg.Multiline(size=(100, 10), key='_update_output_'),
-    ]
-], size=(600, 240))
+        sg.Table(
+            values=[[artist] for artist in ArtistModel().get_artist_names()],
+            max_col_width=40, def_col_width=40, auto_size_columns=False, headings=['Имя'],
+            justification="left", key='_artists_', enable_events=True)
+    ],
+    [sg.Multiline(size=(100, 1), key='_update_output_')]
+])
 
 delete_frame = sg.Frame('Delete', [
     [
@@ -157,7 +164,10 @@ layout = [
 class Gui:
     def __init__(self):
         self.window = sg.Window('Artist Model Redactor', layout, font=("Helvetica, 13"), size=(1300, 740)).Finalize()
+        self.window.move(self.window.CurrentLocation()[0], 0)
         self.artist_model = ArtistModel()
+        self.artists_table = self.window['_artists_']
+        self._selected_artist: str | None = None
 
     def _do_after_time(self, func: Callable, time: float = 1):
         sleep(time)
@@ -258,7 +268,7 @@ class Gui:
         res *= self.validate_links(values)
         return res
 
-    def add_artist(self, values):
+    def _get_artist_data(self, values):
         name = values['_name_']
         year_of_birth = values['_year_of_birth_']
         members_num = values['_members_num_']
@@ -284,26 +294,47 @@ class Gui:
 
         raw_aliases: str = values['_aliases_']
         if raw_aliases:
-            raw_aliases = raw_aliases.replace(',', ' ').replace('  ', ' ')
-            aliases = list(set(raw_aliases.split()))
+            raw_aliases = raw_aliases.replace(' ', '')
+            aliases = raw_aliases.split(',')
+            aliases.append(values['_name_'])
+            aliases = list(set(aliases))
 
+        return name, year_of_birth, members_num, themes, gender, genres, streaming_services_names,\
+               streaming_service_links, aliases
+
+    def add_artist(self, values):
         if not self.validate_input(values):
             self.window['_add_output_'].Update(f'Не удалось добавить артиста: неверные значения полей')
             self.do_after_time(lambda: self.window['_add_output_'].Update(''), 5)
         else:
             try:
-                self.artist_model.add_record(
-                    name, year_of_birth, members_num, themes, gender, genres, streaming_services_names,
-                    streaming_service_links, aliases
-                )
+                self.artist_model.add_record(*self._get_artist_data(values))
+            except AlreadyInTheDatabaseError:
+                self.window['_add_output_'].Update(f'Не удалось добавить: такой артист уже есть в базе')
             except Exception as e:
                 self.window['_add_output_'].Update(f'Не удалось добавить артиста: {e}')
             else:
                 self.window['_add_output_'].Update('')
-                self.window['_add_output_'].Update('Артист добавлен!\n'
-                                                   f'{name} {year_of_birth} {members_num} {themes} '
-                                                   f'{gender} {genres} {streaming_services_names} '
-                                                   f'{streaming_service_links} {aliases}')
+                self.window['_add_output_'].Update(
+                    f"Артист добавлен!\n {' '.join(map(str, self._get_artist_data(values)))}")
+
+    def update_artist(self, values):
+        if not self.validate_input(values):
+            self.window['_update_output_'].Update(f'Не удалось обновить артиста: неверные значения полей')
+            self.do_after_time(lambda: self.window['_update_output_'].Update(''), 5)
+        else:
+            if not self.artist_model.get_by_name(values['_name_']):
+                self.window['_update_output_'].Update(f'Не удалось обновить: артиста с таким именем нет в базе')
+                return
+            
+            try:
+                self.artist_model.add_record(*self._get_artist_data(values), update_if_exist=True)
+            except Exception as e:
+                self.window['_update_output_'].Update(f'Не удалось обновить артиста: {e}')
+            else:
+                self.window['_update_output_'].Update('')
+                self.window['_update_output_'].Update(
+                    f"Артист добавлен!\n {' '.join(map(str, self._get_artist_data(values)))}")
 
     def clean_add_tab_fields(self):
         self.window['_name_'].Update('')
@@ -336,6 +367,60 @@ class Gui:
                 self.window['_delete_output_'].Update('Артист удалён!')
                 self.window['_delete_name_'].Update('')
 
+    @property
+    def selected_artist(self):
+        return self._selected_artist
+
+    @selected_artist.setter
+    def selected_artist(self, name: List[str]):
+        assert isinstance(name, list)
+        assert len(name) > 0
+        assert isinstance(name[0], str)
+        name = name[0]
+        if self._selected_artist != name:
+            self._selected_artist = name
+            self.load_artist(name)
+
+    def _load_artist_data(self, artist: _Artist):
+        self.window['_name_'].Update(artist.name)
+        self.window['_year_of_birth_'].Update(artist.year_of_birth)
+        self.window['_members_num_'].Update(artist.group_members_number)
+
+        if artist.gender == 'male':
+            self.window['_male_radio_'].Update(True)
+        else:
+            self.window['_female_radio_'].Update(True)
+
+        spotify = artist.streaming_service_links.get_link_by_streaming_name('spotify')
+        yandex = artist.streaming_service_links.get_link_by_streaming_name('yandex music')
+        boom = artist.streaming_service_links.get_link_by_streaming_name('boom')
+        if spotify:
+            self.window['_spotify_'].Update(spotify)
+        if yandex:
+            self.window['_yandex_'].Update(yandex)
+        if boom:
+            self.window['_boom_'].Update(boom)
+
+        artist_themes = [theme.name for theme in artist.themes]
+        if 'workout' in artist_themes:
+            workout_index = artist_themes.index('workout')
+            artist_themes[workout_index] = 'workout_theme'
+        [self.window[f'_{theme}_'].Update(True) for theme in artist_themes]
+
+        artist_genres = [genre.name for genre in artist.genres]
+        [self.window[f'_{genre}_'].Update(True) for genre in artist_genres]
+        self.window['_aliases_'].Update(', '.join(artist.aliases))
+
+    def load_artist(self, name):
+        self.clean_add_tab_fields()
+        artist = self.artist_model.get_by_name(name)
+        if artist:
+            self._load_artist_data(artist)
+            self.window['_update_output_'].Update(
+                f'В пространство Artist загружены данные артиста {self.selected_artist}')
+        else:
+            self.window['_update_output_'].Update(f'Не удалось загрузить артиста {self.selected_artist}')
+
     def show(self):
         while True:
             event, values = self.window.read()
@@ -345,11 +430,19 @@ class Gui:
             if not event and not values:
                 continue
 
+            if event == '_artists_':
+                row = values[event]
+                if row:
+                    self.selected_artist = self.window["_artists_"].Values[row[0]]
+
             if event == 'Очистить поля ввода':
                 self.clean_add_tab_fields()
 
             if event == 'Добавить':
                 self.add_artist(values)
+
+            if event == 'Обновить':
+                self.update_artist(values)
 
             if event == 'Удалить артиста':
                 self.delete_artist(values)
