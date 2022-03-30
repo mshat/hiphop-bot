d74d9879d712e9200896435ddf7f3f8c8f995146
Maksim Shatokhin <max.sh99@mail.ru>
2022-03-30 14:26:11 +0300

    fix

--- hiphop_bot/recommender_system/config.py
+++ hiphop_bot/recommender_system/config.py
@@ -1,4 +1,4 @@
-MIN_PROXIMITY = 0.4  # то есть выводит схожих на (1 - MIN_PROXIMITY)%. Чем >, тем < похожие будут попадать в рек-ции
+MIN_PROXIMITY = 0.3  # то есть выводит схожих на (1 - MIN_PROXIMITY)%. Чем >, тем < похожие будут попадать в рек-ции
 GENDER_PROXIMITY_MEASURE_WEIGHT = 0.5
 THEME_PROXIMITY_MEASURE_WEIGHT = 1
 YEAR_OF_BIRTH_PROXIMITY_MEASURE_WEIGHT = 1.1
